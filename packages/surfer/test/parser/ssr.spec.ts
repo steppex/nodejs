858c9eaa6f3a4c2f37bb871c13f4c97dfff4d4e9
justajwolf <changbaihenx@163.com>
2023-09-21 18:22:48 +0800

    feat(packages/surfer): surfer init and add ssr parser

---
+++ packages/surfer/test/parser/ssr.spec.ts
@@ -0,0 +1,11 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { ssrParse } from '../../src';
+
+writeFileSync(
+    `${__dirname}/b.txt`,
+    JSON.stringify(
+        ssrParse(readFileSync(`${__dirname}/a.txt`, { encoding: 'utf8' })),
+        null,
+        4,
+    ),
+);
